314094d7e969c6f64f2a12dc60b02fe0c11b8179
RomaricHaby <haby.romaric@gmail.com>
2021-12-07 10:43:48 +0100

    Add shared preferences manager

---
+++ app/src/main/java/com/example/fdbsilverfilm/manager/SharedPreferencesManager.kt
@@ -0,0 +1,34 @@
+package com.example.fdbsilverfilm.manager
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.fdbsilverfilm.model.Meta
+import com.example.fdbsilverfilm.model.Picture
+import com.google.gson.Gson
+
+object SharedPreferencesManager {
+    private const val CURRENT_META = "current_meta"
+
+    fun saveCurrentMeta(context: Context, picture: Picture){
+        val gson = Gson()
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(CURRENT_META, 0)
+        val editor = sharedPreferences.edit()
+
+        editor.clear()
+
+        val json = gson.toJson(picture.meta)
+
+        editor.putString(CURRENT_META, json)
+
+        editor.apply()
+    }
+
+    fun loadCurrentMeta(context: Context): Meta {
+        val gson = Gson()
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(CURRENT_META, 0)
+
+        val json = sharedPreferences.getString(CURRENT_META, "")
+
+        return gson.fromJson(json, Meta::class.java)
+    }
+}
--- app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.fdbsilverfilm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.fdbsilverfilm.R
+import com.example.fdbsilverfilm.manager.SharedPreferencesManager
+import com.example.fdbsilverfilm.model.Meta
+import com.example.fdbsilverfilm.model.Picture
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
