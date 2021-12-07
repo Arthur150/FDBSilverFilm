059d41bce3e50f9c54b907e2c93a0993933398f5
Arthur <arthur.chevauchet@gmail.com>
2021-12-07 09:42:31 +0100

    init

--- app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fdbsilverfilm
+package com.example.fdbsilverfilm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fdbsilverfilm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
