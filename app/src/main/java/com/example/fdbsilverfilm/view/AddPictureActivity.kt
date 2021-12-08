5b1c9acdd7d5f458901cb81c60db1144bd86b7e6
RomaricHaby <haby.romaric@gmail.com>
2021-12-08 14:55:01 +0100

    Add spinner and find view

--- app/src/main/java/com/example/fdbsilverfilm/view/AddPictureActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/AddPictureActivity.kt
@@ -2,11 +2,36 @@ package com.example.fdbsilverfilm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import com.example.fdbsilverfilm.R
 
 class AddPictureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_picture)
+
+
+        val focal = findViewById<EditText>(R.id.addPictureFocal)
+        val lens = findViewById<EditText>(R.id.addPictureLens)
+        val modeSpinner = findViewById<Spinner>(R.id.addPictureMode)
+        val opening = findViewById<EditText>(R.id.addPictureOpening)
+        val time = findViewById<EditText>(R.id.addPictureTime)
+        val title = findViewById<EditText>(R.id.addPictureTitle)
+
+        val button = findViewById<Button>(R.id.addPictureButton)
+
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.picture_mode,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            modeSpinner.adapter = adapter
+        }
     }
 }
