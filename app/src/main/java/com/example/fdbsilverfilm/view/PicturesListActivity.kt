cd490daa0b1fd4ba4656aeac07bc251ac0353470
Antoine <antoine_pannecoucke@orange.fr>
2021-12-08 11:26:27 +0100

    Adapter

--- app/src/main/java/com/example/fdbsilverfilm/view/PicturesListActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/PicturesListActivity.kt
@@ -2,7 +2,10 @@ package com.example.fdbsilverfilm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fdbsilverfilm.R
+import com.example.fdbsilverfilm.adapter.PicturesListAdapter
 import com.example.fdbsilverfilm.model.Film
 import com.example.fdbsilverfilm.model.Globals
 import com.example.fdbsilverfilm.viewmodel.PicturesListViewModel
@@ -16,6 +19,9 @@ class PicturesListActivity : AppCompatActivity() {
 
         vm = PicturesListViewModel(intent.getSerializableExtra(Globals.FILM_EXTRA_TAG) as Film)
 
-        
+        val recyclerView : RecyclerView = findViewById(R.id.pictures_list_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = PicturesListAdapter(vm?.data, this)
+
     }
 }
