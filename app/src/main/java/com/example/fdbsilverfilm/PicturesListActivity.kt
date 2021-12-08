f43ff128e457b2ddafbc0001382f85e893421235
Antoine <antoine_pannecoucke@orange.fr>
2021-12-08 09:29:32 +0100

    Vue

---
+++ app/src/main/java/com/example/fdbsilverfilm/PicturesListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fdbsilverfilm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PicturesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pictures_list)
+    }
+}
