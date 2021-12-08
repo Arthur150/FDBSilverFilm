64971d30672c15ac2a43e94d86d45eef68df150d
RomaricHaby <haby.romaric@gmail.com>
2021-12-08 14:38:24 +0100

    add layout add picture

---
+++ app/src/main/java/com/example/fdbsilverfilm/view/AddPictureActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fdbsilverfilm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fdbsilverfilm.R
+
+class AddPictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_picture)
+    }
+}
--- app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.fdbsilverfilm.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fdbsilverfilm.R
 import com.example.fdbsilverfilm.model.Film
 
@@ -13,8 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this,AddFilmActivity::class.java)
-        intent.putExtra("filmToEdit", Film(0,"dqdqdq","dqsdqsdqsd",5,"Black and white",25))
+        /*val intent = Intent(this, AddFilmActivity::class.java)
+         intent.putExtra("filmToEdit", Film(0, "dqdqdq", "dqsdqsdqsd", 5, "Black and white", 25))
+         startActivity(intent)*/
+
+        val intent = Intent(this, AddPictureActivity::class.java)
+        intent.putExtra("filmToEdit", Film(0, "dqdqdq", "dqsdqsdqsd", 5, "Black and white", 25))
         startActivity(intent)
 
     }
