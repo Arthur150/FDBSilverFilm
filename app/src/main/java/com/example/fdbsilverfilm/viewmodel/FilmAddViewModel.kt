cb25c932732bb94e28d25d1eac5336e9e3e246c2
RomaricHaby <haby.romaric@gmail.com>
2021-12-08 10:28:56 +0100

    fix bug with constructor film

--- app/src/main/java/com/example/fdbsilverfilm/viewmodel/FilmAddViewModel.kt
+++ app/src/main/java/com/example/fdbsilverfilm/viewmodel/FilmAddViewModel.kt
@@ -3,14 +3,14 @@ package com.example.fdbsilverfilm.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.fdbsilverfilm.model.Film
 
-class FilmAddViewModel(var film: Film = Film()) : ViewModel() {
+class FilmAddViewModel(var film: Film = Film(null)) : ViewModel() {
 
     fun setFilm(iso: Int, nbPoses: Int, brand: String, type: String, name: String? = null) {
         film.name = name
         film.brand = brand
         film.iso = iso
         film.type = type
-        film.nbPoses = nbPoses
+        film.ndPoses = nbPoses
     }
 
 }
