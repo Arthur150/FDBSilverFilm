874300643b137fc78440ea2bc678f8f477d4233b
Arthur <arthur.chevauchet@gmail.com>
2021-12-08 14:52:42 +0100

    add film button

--- app/src/main/java/com/example/fdbsilverfilm/view/FilmListActivity.kt
+++ app/src/main/java/com/example/fdbsilverfilm/view/FilmListActivity.kt
@@ -1,12 +1,15 @@
 package com.example.fdbsilverfilm.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fdbsilverfilm.R
 import com.example.fdbsilverfilm.adapter.FilmAdapter
 import com.example.fdbsilverfilm.viewmodel.FilmListViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FilmListActivity : AppCompatActivity() {
 
@@ -16,6 +19,12 @@ class FilmListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_list)
+
+        val addButton = findViewById<FloatingActionButton>(R.id.filmListAddButton)
+        addButton.setOnClickListener {
+            val intent = Intent(this,AddFilmActivity::class.java)
+            startActivity(intent)
+        }
 
         val recyclerView = findViewById<RecyclerView>(R.id.filmListRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
